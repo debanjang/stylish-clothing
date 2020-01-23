86690b3d453a2ad91ed5b0e246d8cef25595d2f3
Debanjan Ghosh <debanjang@ymail.com>
2020-01-23 22:54:51 +0530

    Added Shop Page and Collection components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
-import Hats from "./pages/hats/hats.component";
+import ShopPage from "./pages/shop/shop.component";
 import "./App.css";
-import Jackets from "./pages/jackets/jackets.component";
-import Sneakers from "./pages/sneakers/sneakers.component";
-import Womens from "./pages/womens/womens.component";
-import Mens from "./pages/mens/mens.component";
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop/hats" component={Hats} />
-      <Route exact path="/shop/jackets" component={Jackets} />
-      <Route exact path="/shop/sneakers" component={Sneakers} />
-      <Route exact path="/shop/womens" component={Womens} />
-      <Route exact path="/shop/mens" component={Mens} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
