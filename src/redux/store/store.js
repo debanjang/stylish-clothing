c4b00c167b9efb3f12786c26d41c1191b82a1285
Debanjan Ghosh <debanjang@ymail.com>
2020-05-26 11:51:59 +0530

    Added local storage with redux-persist

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-import { rootReducer } from "../root-reducer/root.reducer";
+import rootReducer from "../root-reducer/root.reducer";
+import { persistStore } from "redux-persist";
 
 const middleWares = [logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middleWares));
+export const persistor = persistStore(store);
